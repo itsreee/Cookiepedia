e210fb5374020f932cac42ea8fb0cec0c5d0030e
itsreee <sreelakshmismenon8086@gmail.com>
2024-12-03 10:11:11 +0530

    frontend

--- src/app/recipies/recipies.component.ts
+++ src/app/recipies/recipies.component.ts
@@ -11,8 +11,10 @@ import { ApiService } from '../services/api.service';
   styleUrl: './recipies.component.css'
 })
 export class RecipiesComponent {
-  allRecipes:any = [],
+  allRecipes:any = []
   cuisineArray:any = []
+  mealTypeArray:any=[]
+
   constructor(private api:ApiService){}
   ngOnInit(){
     this.getAllRecipes()
@@ -22,8 +24,19 @@ export class RecipiesComponent {
     this.api.getAllRecipesAPI().subscribe((res:any)=>{
     this.allRecipes=res
     console.log(this.allRecipes);
-    this.allRecipes.forEach((item:any))=>{
-      !this.cuisineArray.includes(item.cuisine)&& this.cuisineArray.push(item.cuisine)    }
+    this.allRecipes.forEach((item:any)=>{
+      !this.cuisineArray.includes(item.cuisine)&& this.cuisineArray.push(item.cuisine)   
+ 
+    })
+    console.log(this.cuisineArray);
+    const dummyMeal = this.allRecipes.map((item:any)=>item.mealType)
+    console.log(dummyMeal.flat(Infinity));
+    const flatDummyArray = dummyMeal.flat(Infinity)
+    flatDummyArray.forEach((item:any)=>{
+    !this.mealTypeArray.includes(item)&& this.mealTypeArray.push(item)    
+    })
+    console.log(this.mealTypeArray);
+    
     })
   }
 
